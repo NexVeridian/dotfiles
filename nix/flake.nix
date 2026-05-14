e58e8d8e74d862638511be8fc4609bb5a833c36a
NexVeridian <elijah@drswarm.com>
2026-05-14 10:41:17 -0700

    pnpm

--- nix/flake.nix
+++ nix/flake.nix
@@ -212,7 +212,12 @@
           environment.variables = {
             LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ openssl ];
             DYLD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [ openssl ];
+            PNPM_HOME = "$HOME/.local/share/pnpm";
           };
+
+          programs.zsh.shellInit = ''
+            export PATH="$PNPM_HOME/bin:$PATH"
+          '';
 
           # Enable alternative shell support in nix-darwin.
           programs.zsh.enable = true;
